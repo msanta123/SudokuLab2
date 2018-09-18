5f7dd1d8b1a549db2d82a5d3b8ddab93e46baeca
Matthew <Matthew@DESKTOP-H30SUIU>
2018-09-18 09:49:28 -0400

    added getPuzzle and the skeletons of other methods

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -6,4 +6,37 @@ public class Sudoku extends LatinSquare{
 	
 	private int iSize;
 	private int iSqrtSize;
+	
+	
+	public Sudoku() {
+		super();
+	}
+	public Sudoku(int[][] latinSquare) {
+		super(latinSquare);
+	}
+	protected int[][] getPuzzle()
+	{
+		return super.getLatinSquare();
+	}
+	protected int[] getRegion(int iRegionNbr)
+	{
+		return null;
+	}
+	protected int[] getRegion(int Col, int Row)
+	{
+		return null;
+	}
+	protected boolean isSudoku()
+	{
+		return false;
+	}
+	protected boolean isPartialSudoku()
+	{
+		return false;
+	}
+	protected boolean isValueValid(int iValue, int Col, int Row)
+	{
+		return false;
+	}
 }
+
