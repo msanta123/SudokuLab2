d7f32b18573fba24235cfb1a80eeb472b8f398e9
Matthew <Matthew@DESKTOP-H30SUIU>
2018-09-18 09:35:16 -0400

    Imports Latinsquare

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -1,5 +1,9 @@
 package pkgGame;
+import java.util.Arrays;
+import pkgHelper.LatinSquare;
 
-public class Sudoku {
-
+public class Sudoku extends LatinSquare{
+	
+	private int iSize;
+	private int iSqrtSize;
 }
