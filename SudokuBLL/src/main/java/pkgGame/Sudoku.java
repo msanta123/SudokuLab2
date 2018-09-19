ccd698c609ffaaf0966705ad7e15cf10da820ff4
Matthew <Matthew@DESKTOP-H30SUIU>
2018-09-19 19:24:56 -0400

    I think getRegion may work

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -1,5 +1,5 @@
 package pkgGame;
-import java.util.Arrays;
+
 import pkgHelper.LatinSquare;
 
 public class Sudoku extends LatinSquare{
@@ -20,10 +20,31 @@ public class Sudoku extends LatinSquare{
 	}
 	protected int[] getRegion(int iRegionNbr)
 	{
-		return null;
+		/*int[] a = super.getRow(0);
+		int[] b = super.getRow(1);
+		int[] c = super.getRow(2);*/
+		int[] a = {1,2,3};
+		int[] b = {4,5,6};
+		int[] c = {7,8,9};
+		int[] region0 = {0,0,0,0,0,0,0,0,0};
+		for(int i = 0; i < 3; i++) {
+			region0[i] = a[i]; 
+		}
+		for(int i = 3; i < 6; i++) {
+			for(int j = 0; j < 3; j++) {
+				region0[i] = b[j];
+			}
+		}
+		for(int i = 6; i < 9; i++) {
+			for(int j = 0; j < 3; j++) {
+				region0[i] = c[j];
+			}
+		}
+		return region0;
 	}
 	protected int[] getRegion(int Col, int Row)
 	{
+		
 		return null;
 	}
 	protected boolean isSudoku()
